ddff919c135f4458086e297692672c5c0fcd0d29
alexandra <alexandra_mariaa27@yahoo.com>
2019-09-06 13:19:17 +0300

    Add project files.

---
+++ SingletonProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton fromEmployee = Singleton.GetInstance;
+            fromEmployee.PrintDetails("FromEmployee");
+
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PrintDetails("FromStudent");
+            Console.ReadLine();
+        }
+    }
+}
